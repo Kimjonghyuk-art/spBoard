3f4be353d51d9907611378f4bcf48b0b968c4aef
jonghyuk <kasiruske@naver.com>
2022-11-03 18:03:12 +0900

    modify project add lombok

--- boardex/src/main/java/com/app/board/vo/BoardFileVO.java
+++ boardex/src/main/java/com/app/board/vo/BoardFileVO.java
@@ -2,6 +2,8 @@ package com.app.board.vo;
 
 import java.util.Date;
 
+import lombok.Data;
+@Data
 public class BoardFileVO {
 	
 	// Fields
@@ -13,47 +15,6 @@ public class BoardFileVO {
 	private String filePath;
 	private int idx;
 	
-	// Getters & Setters
-	public int getFileNo() {
-		return fileNo;
-	}
-	public void setFileNo(int fileNo) {
-		this.fileNo = fileNo;
-	}
-	public String getFileOriginalName() {
-		return fileOriginalName;
-	}
-	public void setFileOriginalName(String fileOriginalName) {
-		this.fileOriginalName = fileOriginalName;
-	}
-	public String getFileStoredName() {
-		return fileStoredName;
-	}
-	public void setFileStoredName(String fileStoredName) {
-		this.fileStoredName = fileStoredName;
-	}
-	public String getFileContentType() {
-		return fileContentType;
-	}
-	public void setFileContentType(String fileContentType) {
-		this.fileContentType = fileContentType;
-	}
-	public Date getFileUploadDate() {
-		return fileUploadDate;
-	}
-	public void setFileUploadDate(Date fileUploadDate) {
-		this.fileUploadDate = fileUploadDate;
-	}
-	public String getFilePath() {
-		return filePath;
-	}
-	public void setFilePath(String filePath) {
-		this.filePath = filePath;
-	}
-	public int getIdx() {
-		return idx;
-	}
-	public void setIdx(int idx) {
-		this.idx = idx;
-	}
+	
+	
 }
--- boardex/src/main/java/com/app/board/vo/BoardVO.java
+++ boardex/src/main/java/com/app/board/vo/BoardVO.java
@@ -2,6 +2,9 @@ package com.app.board.vo;
 
 import com.app.board.common.Pagination;
 
+import lombok.Data;
+
+@Data
 public class BoardVO extends Pagination{
 
 	private int idx;
@@ -13,56 +16,6 @@ public class BoardVO extends Pagination{
 	private int likeNo;
 	private int fileNo;
 	
-	
-	public int getFileNo() {
-		return fileNo;
-	}
-	public void setFileNo(int fileNo) {
-		this.fileNo = fileNo;
-	}
-	public int getLikeNo() {
-		return likeNo;
-	}
-	public void setLikeNo(int likeNo) {
-		this.likeNo = likeNo;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	
-	public int getIdx() {
-		return idx;
-	}
-	public void setIdx(int idx) {
-		this.idx = idx;
-	}
-	public String getWriter() {
-		return writer;
-	}
-	public void setWriter(String writer) {
-		this.writer = writer;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getCreateDate() {
-		return createDate;
-	}
-	public void setCreateDate(String createDate) {
-		this.createDate = createDate;
-	}
-	public int getHit() {
-		return hit;
-	}
-	public void setHit(int hit) {
-		this.hit = hit;
-	}
 	
 	
 	
